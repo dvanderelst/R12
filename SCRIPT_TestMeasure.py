4060e0f1a6d73c9512783d33f4d085ce8e7ced1a
Dieter <dieter.vanderelst@fastmail.net>
2024-08-19 08:25:38 -0400

    merged

--- SCRIPT_TestMeasure.py
+++ SCRIPT_TestMeasure.py
@@ -3,9 +3,6 @@ import random
 import time
 R = RobotBat.RobotBat(connect_robot=False, connect_sonar=True)
 
-for i in range(3):
-    if i%100 == 0:
-        R.measure(plot=True)
-    else:
-        R.measure(plot=False)
-    time.sleep(random.random())
+for i in range(1000):
+    R.measure(plot=False)
+    time.sleep(0.1)
